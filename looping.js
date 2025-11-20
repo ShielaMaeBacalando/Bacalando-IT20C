cd20265ef50f319aac66379dca46a24557957db7
ShielaMaeBacalando <chooksterchookie@gmail.com>
2025-11-20 08:33:21 +0800

    Added for loop.js

--- looping.js
+++ looping.js
@@ -7,4 +7,12 @@ for (i = 10; i <= 5; i++) {
 
 }
 console.log(output);
+// Arrays
+output = " ";
+arr = [1, 2, 3]
+console.log("Array length:" + arr.length)
+for (index = arr.length -1; index>=0; i--) {
+    ouput += arr[index] + " ";
+}
+console.log(output);
 
