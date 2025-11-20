5b383aca7473432abd699efb4ffe02ccb1ef21a3
ShielaMaeBacalando <chooksterchookie@gmail.com>
2025-11-20 08:39:10 +0800

    correct array looping in while-loop

--- while-loop.js
+++ while-loop.js
@@ -1,15 +1,26 @@
-console.log("Descending");
-i = 5;
+// console.log("Descending");
+// i = 5;
 
-while (i >= 1) {
-    console.log(i);
-    i--;
-}
-console.log("Ascending");
-x=1;
+// while (i >= 1) {
+//     console.log(i);
+//     i--;
+// }
+// console.log("Ascending");
+// x=1;
 
-while (x <= 5) {
-    console.log(x);
-    x++;
+// while (x <= 5) {
+//     console.log(x);
+//     x++;
+// }
+
+arr=[4,2,3];
+output = " ";
+index = 0;
+
+while(index <= arr.length-1){
+    output += arr[index] + " ";
+    index++;
 }
+console.log(output);
+
 
